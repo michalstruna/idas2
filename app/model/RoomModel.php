e978cf92cc3aa20144ae2e71d59a87ba0d70b0fc
ST52530 <st52530@student.upce.cz>
2018-11-25 21:49:44 +0100

    Create models

---
+++ app/model/RoomModel.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tomas
+ * Date: 25/11/2018
+ * Time: 21:37
+ */
+
+namespace App\Model;
+
+
+class RoomModel extends BaseModel implements IDatabaseWrapper {
+
+    public function getAll(): array
+    {
+        // TODO: Implement getAll() method.
+    }
+
+    public function getById(string $id)
+    {
+        // TODO: Implement getById() method.
+    }
+
+    public function updateById(string $id, array $changes): void
+    {
+        // TODO: Implement updateById() method.
+    }
+
+    public function deleteById(string $id): void
+    {
+        // TODO: Implement deleteById() method.
+    }
+
+    public function insert(array $item): void
+    {
+        // TODO: Implement insert() method.
+    }
+}
