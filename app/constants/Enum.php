40bb454c0d9d7148c824cdce74e75dae326e1f16
Michal Struna <michal.l.struna@gmail.com>
2018-12-01 14:10:53 +0100

    Add days enum.

---
+++ app/constants/Enum.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Michal
+ * Date: 01/12/2018
+ * Time: 13:32
+ */
+
+namespace App\Constants;
+
+use ReflectionClass;
+
+Trait Enum {
+
+    static function getConstants() {
+        $reflection = new ReflectionClass(__CLASS__);
+        return $reflection->getConstants();
+    }
+
+}
