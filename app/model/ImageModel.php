c9e5e890d8802241905238f35e465e7ea66a021a
ST52530 <st52530@student.upce.cz>
2018-12-03 12:05:46 +0100

    Load oci parameters from config

--- app/model/ImageModel.php
+++ app/model/ImageModel.php
@@ -8,7 +8,21 @@
 
 namespace App\Model;
 
+use Nette\Database\Connection;
+
 class ImageModel extends BaseModel {
+
+    public $ociLogin;
+
+    /**
+     * ImageModel constructor.
+     * @param Connection $database
+     * @param OciLoginProvider $ociLoginProvider
+     */
+    public function __construct(Connection $database, OciLoginProvider $ociLoginProvider) {
+        parent::__construct($database);
+        $this->ociLogin = $ociLoginProvider->ociLogin;
+    }
 
     function getById($id) {
         return $this->database->fetch('SELECT * FROM SEM_OBRAZEK WHERE ID = ?', $id);
@@ -17,16 +31,15 @@ class ImageModel extends BaseModel {
     function insert($image, $suffix) {
         $sql = "INSERT INTO SEM_OBRAZEK (id, obrazek, pripona, vytvoreno) VALUES (SEM_OBRAZEK_SEQ.NEXTVAL, empty_blob(), 'test', SYSDATE) RETURNING obrazek INTO :obrazek";
 
-        $connection = oci_connect('C##xxx', 'xxx', '(DESCRIPTION = (ADDRESS = (PROTOCOL = TCP)(HOST = fei-sql1.upceucebny.cz)(PORT = 1521)) (CONNECT_DATA = (SID = IDAS12)))');
+        $connection = oci_connect($this->ociLogin['user'], $this->ociLogin['password'], $this->ociLogin['connection']);
         $result = oci_parse($connection, $sql);
         $blob = oci_new_descriptor($connection, OCI_D_LOB);
         oci_bind_by_name($result, ":obrazek", $blob, -1, OCI_B_BLOB);
         oci_execute($result, OCI_DEFAULT) or die ("Unable to execute query");
 
-        if(!$blob->save($image)) {
+        if (!$blob->save($image)) {
             oci_rollback($connection);
-        }
-        else {
+        } else {
             oci_commit($connection);
         }
 
