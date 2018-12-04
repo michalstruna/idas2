521b3995738ccd55c93ec40848ad8486af851bc6
ST52530 <st52530@student.upce.cz>
2018-12-04 09:25:20 +0100

    Fix triggers

--- database/sql/create_triggers.sql
+++ database/sql/create_triggers.sql
@@ -1,33 +1,17 @@
 create or replace TRIGGER SEM_SMAZ_OBRAZEK
-AFTER UPDATE of obrazek_id ON sem_ucitel
+AFTER UPDATE of obrazek_id OR DELETE ON sem_ucitel
 for each row
 BEGIN
-DELETE FROM SEM_OBRAZEK WHERE ID = :old.obrazek_id;
+  DELETE FROM SEM_OBRAZEK WHERE ID = :old.obrazek_id;
 END;
 
 /
 
-create or replace TRIGGER SEM_SMAZ_OBRAZEK_DELETE
-AFTER DELETE ON sem_ucitel
-for each row
-BEGIN
-DELETE FROM SEM_OBRAZEK WHERE ID = :old.obrazek_id;
-END;
-
-/
-
--- TODO
 CREATE OR REPLACE TRIGGER sem_nastav_den_v_tydnu
-BEFORE UPDATE OR INSERT ON sem_rozvrh
+BEFORE INSERT OR UPDATE ON sem_rozvrh
 FOR EACH ROW
 BEGIN
-  DBMS_OUTPUT.PUT_LINE(:NEW.presne_datum);
-  DBMS_OUTPUT.PUT_LINE(TO_CHAR(TO_DATE(:NEW.presne_datum, 'YYYY-MM-DD'), 'D') - 1);
-
-  DBMS_OUTPUT.PUT_LINE(TO_DATE('2018-12-04', 'YYYY-MM-DD'));
-  DBMS_OUTPUT.PUT_LINE(TO_CHAR(TO_DATE('2018-12-04', 'YYYY-MM-DD'), 'D') - 1);
-
-  --IF :OLD.presne_datum IS NOT NULL THEN
-    --:NEW.den_v_tydnu := TO_CHAR(:OLD.presne_datum, 'D') - 1;
-  --END IF;
+  IF :NEW.presne_datum IS NOT NULL THEN
+    :NEW.den_v_tydnu := TO_CHAR(:NEW.presne_datum, 'D') - 1;
+  END IF;
 END;
