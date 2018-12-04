353785c8b17a8dc047944845036b8de0aa223baf
ST52530 <st52530@student.upce.cz>
2018-12-04 10:50:26 +0100

    Fix trigger for real

--- database/sql/create_triggers.sql
+++ database/sql/create_triggers.sql
@@ -12,6 +12,6 @@ BEFORE INSERT OR UPDATE ON sem_rozvrh
 FOR EACH ROW
 BEGIN
   IF :NEW.presne_datum IS NOT NULL THEN
-    :NEW.den_v_tydnu := TO_CHAR(:NEW.presne_datum, 'D') - 1;
+    :NEW.den_v_tydnu := TRUNC(TO_DATE(:NEW.presne_datum, 'YYYY-MM-DD')) - TRUNC (TO_DATE(:NEW.presne_datum, 'YYYY-MM-DD'), 'IW');
   END IF;
 END;
