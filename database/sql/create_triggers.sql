1549da4d6b2622a84d7c636e3d7834f40e510480
ST52530 <st52530@student.upce.cz>
2018-12-04 10:56:59 +0100

    Fix trigger, add function

--- database/sql/create_triggers.sql
+++ database/sql/create_triggers.sql
@@ -12,6 +12,6 @@ BEFORE INSERT OR UPDATE ON sem_rozvrh
 FOR EACH ROW
 BEGIN
   IF :NEW.presne_datum IS NOT NULL THEN
-    :NEW.den_v_tydnu := TRUNC(TO_DATE(:NEW.presne_datum, 'YYYY-MM-DD')) - TRUNC (TO_DATE(:NEW.presne_datum, 'YYYY-MM-DD'), 'IW');
+    :NEW.den_v_tydnu := SEM_DATUM_NA_DEN(:NEW.presne_datum);
   END IF;
 END;
