85e66e6e6e9b445dcf76942f70870f66bac80c62
ST52530 <st52530@student.upce.cz>
2018-12-08 19:41:08 +0100

    Allow editing schedule to teachers

--- app/controls/ScheduleControl.php
+++ app/controls/ScheduleControl.php
@@ -30,8 +30,9 @@ class ScheduleControl extends Control {
         $template->handleEdit = $this->presenter->getName() . ':edit';
         $template->handleDelete = $this->presenter->getName() . ':delete';
         $template->handleAdd = $this->presenter->getName() . ':add';
-        $template->isEditVisible = $this->presenter->getUser()->isInRole('admin');
-        $template->isAddVisible = $template->isEditVisible || $this->presenter->getUser()->isInRole('teacher'); // TODO: Is owner.
+        $template->isAdmin = $this->presenter->getUser()->isInRole('admin');
+        $template->teacherId = $this->presenter->getUser()->getIdentity()->teacherId;
+        $template->isAddVisible = $template->isAdmin || $this->presenter->getUser()->isInRole('teacher');
 
         $template->getDayNameByIndex = function ($index): string {
             return Days::findByNestedKey('index', (int)$index)['text'];
